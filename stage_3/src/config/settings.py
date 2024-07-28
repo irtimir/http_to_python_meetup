6c1816318477ee2adbc2cec340c9cbdfef05b383
ilias <irtimirbulatov@gmail.com>
2024-07-28 11:53:47 +0300

    django debug -> false + serve static with debug

--- stage_3/src/config/settings.py
+++ stage_3/src/config/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xj3rwm+k-3^0bn7tdox0%9&powd*c-0@r55s(_-ooc8#u)7!uq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
--- stage_3/src/config/urls.py
+++ stage_3/src/config/urls.py
@@ -15,11 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import serve
 
 from core.views import test_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', test_view)
+    path('test/', test_view),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(f'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
