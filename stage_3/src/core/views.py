b0a4e29583c0cbf9e8b0ffd8f126e5c99e40f393
ilias <irtimirbulatov@gmail.com>
2024-07-31 12:28:48 +0300

    stage3: init container for backend

--- stage_3/src/core/views.py
+++ stage_3/src/core/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 
@@ -5,4 +7,4 @@ from django.http import HttpResponse
 def test_view(request: WSGIRequest) -> HttpResponse:
     print(type(request))
     print(request.environ)
-    return HttpResponse(content=b'OK')
+    return HttpResponse(content=str(random.randint(0, 100)).encode())
